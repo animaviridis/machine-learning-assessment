a0c96f9cb0a17f3793f0929b6cede9302961fa16
mysh <mysh995@gmail.com>
2019-10-27 22:51:00 +0000

    [Regression] Split dataset

--- regression.py
+++ regression.py
@@ -6,5 +6,9 @@ import pandas as pd
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-df = pd.read_excel(config['Data']['data_file'])
-print(df.head())
+# split dataset into
+df_input = pd.read_excel(config['Data']['data_file'])
+target_var = df_input.keys()[int(config['Data']['target_column'])]
+df_target = df_input.loc[:, target_var]
+df_input.drop(target_var, axis=1, inplace=True)
+print(df_input.head())
