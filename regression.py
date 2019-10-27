85b6a93fe3b048ed12f9a0f575b82380d11221f4
mysh <mysh995@gmail.com>
2019-10-27 23:19:34 +0000

    [Regression] Logging

--- regression.py
+++ regression.py
@@ -1,14 +1,27 @@
 import configparser
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+import logging
 
 
 # read the configuration file
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-# split dataset into
-df_input = pd.read_excel(config['Data']['data_file'])
+# configure logger
+logging.basicConfig(level=config['Logging']['level'])
+logger = logging.getLogger(__name__)
+
+
+# load data dataset
+data_fname = config['Data']['data_file']
+logger.info(f"Loading data from file {data_fname}")
+df_input = pd.read_excel(data_fname)
+logger.info("Loading completed")
+print(df_input.head())
+
+# split dataset into input and target
 target_var = df_input.keys()[int(config['Data']['target_column'])]
 df_target = df_input.loc[:, target_var]
 df_input.drop(target_var, axis=1, inplace=True)
-print(df_input.head())
+
