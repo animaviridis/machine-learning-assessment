0b8f0cde3bbddbbbb8d16d36d35829d7a7b8568f
mysh <mysh995@gmail.com>
2019-11-05 15:31:56 +0000

    [DT] Tree level and str representation

--- assignment1/task2/decisiontree.py
+++ assignment1/task2/decisiontree.py
@@ -2,20 +2,35 @@
 
 
 class Node(object):
-    def __init__(self, data, indices=None, parent=None):
+    def __init__(self, data, level=0, indices=None, terminal=False, parent=None):
         self._data = data
+        self._level = level
+        self.terminal = terminal
 
         indices = set(indices) if indices is not None else set(range(len(data)))
 
         self._indices_distributed = set()
         self._indices_remaining = indices
 
+        if level and not parent:
+            raise ValueError(f"Non-root node (level {level}) must have a parent")
+
         self._parent = parent
         self._children = set()
+
+    def __str__(self):
+        root = '' if self.level else ' (root)'
+        leaf = '(leaf)' if self.terminal else f'with {len(self.children)} children'
+
+        return f"Level {self.level} tree node{root}; {'' if self.resolved else 'not '}resolved {leaf}"
 
     @property
     def data(self):
         return self._data
+
+    @property
+    def level(self):
+        return self._level
 
     @property
     def indices_distributed(self):
@@ -30,6 +45,10 @@ class Node(object):
         return self._indices_distributed | self._indices_remaining  # set union
 
     @property
+    def resolved(self):
+        return True if (self.terminal or not self._indices_remaining) else False
+
+    @property
     def parent(self):
         return self._parent
 
@@ -37,7 +56,7 @@ class Node(object):
     def children(self):
         return self._children
 
-    def add_child(self, child):
+    def _add_child(self, child):
         if not isinstance(child, type(self)):
             raise TypeError(f"Child should be of type {type(self)}")
 
@@ -50,7 +69,10 @@ class Node(object):
         self._indices_remaining -= chis     # set difference
         self._children.add(child)
 
-    def new_child(self, indices):
-        child = self.__class__(self._data, indices, parent=self)
-        self.add_child(child)
+    def add_new_child(self, indices):
+        child = self.__class__(self._data, level=self.level+1, indices=indices, parent=self)
+        self._add_child(child)
+
+    def add_final_child(self):
+        self.add_new_child(self.indices_remaining)
 
