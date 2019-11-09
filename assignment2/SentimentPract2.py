c17ddac9f1fa870c5ec01aa754828ddae5a08ed2
mysh <mysh995@gmail.com>
2019-11-09 21:02:32 +0000

    [Sentiments] formatting changes

--- assignment2/SentimentPract2.py
+++ assignment2/SentimentPract2.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
-import re, random, math, collections, itertools
+import re
+import random
+import math
+import collections
+import itertools
 
-PRINT_ERRORS=0
+PRINT_ERRORS = 0
 
-#------------- Function Definitions ---------------------
+# ------------- Function Definitions ---------------------
 
 
 def readFiles(sentimentDictionary,sentencesTrain,sentencesTest,sentencesNokia):
@@ -29,38 +33,40 @@ def readFiles(sentimentDictionary,sentencesTrain,sentencesTest,sentencesNokia):
     negDictionary = open('negative-words.txt', 'r', encoding="ISO-8859-1")
     negWordList = negDictionary.readlines()
     negWordList = [line.strip() for line in negWordList if not line.startswith(";") and not line == '\n']
-    #negWordList = re.findall(r"[a-z\-]+", negDictionary.read())
+    # negWordList = re.findall(r"[a-z\-]+", negDictionary.read())
 
     for i in posWordList:
         sentimentDictionary[i] = 1
     for i in negWordList:
         sentimentDictionary[i] = -1
 
-    #create Training and Test Datsets:
-    #We want to test on sentences we haven't trained on, to see how well the model generalses to previously unseen sentences
+    # create Training and Test Datsets:
+    # We want to test on sentences we haven't trained on, to see how well
+    # the model generalses to previously unseen sentences
 
-  #create 90-10 split of training and test data from movie reviews, with sentiment labels    
+    # create 90-10 split of training and test data from movie reviews, with sentiment labels    
     for i in posSentences:
-        if random.randint(1,10)<2:
-            sentencesTest[i]="positive"
+        if random.randint(1, 10) < 2:
+            sentencesTest[i] = "positive"
         else:
-            sentencesTrain[i]="positive"
+            sentencesTrain[i] = "positive"
 
     for i in negSentences:
-        if random.randint(1,10)<2:
-            sentencesTest[i]="negative"
+        if random.randint(1, 10) <2:
+            sentencesTest[i] = "negative"
         else:
-            sentencesTrain[i]="negative"
+            sentencesTrain[i] = "negative"
 
-    #create Nokia Datset:
+    # create Nokia Datset:
     for i in posSentencesNokia:
-            sentencesNokia[i]="positive"
+            sentencesNokia[i] = "positive"
     for i in negSentencesNokia:
-            sentencesNokia[i]="negative"
+            sentencesNokia[i] = "negative"
 
-#----------------------------End of data initialisation ----------------#
+# ----------------------------End of data initialisation ----------------#
 
-#calculates p(W|Positive), p(W|Negative) and p(W) for all words in training data
+
+# calculates p(W|Positive), p(W|Negative) and p(W) for all words in training data
 def trainBayes(sentencesTrain, pWordPos, pWordNeg, pWord):
     posFeatures = [] # [] initialises a list [array]
     negFeatures = [] 
@@ -71,49 +77,50 @@ def trainBayes(sentencesTrain, pWordPos, pWordNeg, pWord):
     negWordsTot = 0
     allWordsTot = 0
 
-    #iterate through each sentence/sentiment pair in the training data
+    # iterate through each sentence/sentiment pair in the training data
     for sentence, sentiment in sentencesTrain.items():
         wordList = re.findall(r"[\w']+", sentence)
 
-        #TO DO:
-        #Populate bigramList (initialised below) by concatenating adjacent words in the sentence.
-        #You might want to seperate the words by _ for readability, so bigrams such as:
-        #You_might, might_want, want_to, to_seperate.... 
+        # TODO:
+        # Populate bigramList (initialised below) by concatenating adjacent words in the sentence.
+        # You might want to seperate the words by _ for readability, so bigrams such as:
+        # You_might, might_want, want_to, to_seperate.... 
 
-        bigramList=wordList.copy() #initialise bigramList
+        bigramList = wordList.copy()  # initialise bigramList
         for x in range(len(wordList)-1):
            bigramList.append(wordList[x]+"_" + wordList[x+1])
-
-
  
-        #-------------Finish populating bigramList ------------------#
+        # -------------Finish populating bigramList ------------------#
         
-        #TO DO: when you have populated bigramList, uncomment out the line below and , and comment out the unigram line to make use of bigramList rather than wordList
+        # TODO: 
+        #  when you have populated bigramList, 
+        #  uncomment out the line below and , 
+        #  and comment out the unigram line to make use of bigramList rather than wordList
         
-        for word in bigramList: #calculate over bigrams
-        # for word in wordList: #calculate over unigrams
-            allWordsTot += 1 # keeps count of total words in dataset
+        for word in bigramList:  # calculate over bigrams
+        # for word in wordList:  # calculate over unigrams
+            allWordsTot += 1    # keeps count of total words in dataset
             if not (word in dictionary):
                 dictionary[word] = 1
-            if sentiment=="positive" :
-                posWordsTot += 1 # keeps count of total words in positive class
+            if sentiment == "positive":
+                posWordsTot += 1  # keeps count of total words in positive class
 
-                #keep count of each word in positive context
+                # keep count of each word in positive context
                 if not (word in freqPositive):
                     freqPositive[word] = 1
                 else:
                     freqPositive[word] += 1    
             else:
-                negWordsTot+=1# keeps count of total words in negative class
+                negWordsTot += 1  # keeps count of total words in negative class
                 
-                #keep count of each word in positive context
+                #  keep count of each word in positive context
                 if not (word in freqNegative):
                     freqNegative[word] = 1
                 else:
                     freqNegative[word] += 1
 
     for word in dictionary:
-        #do some smoothing so that minimum count of a word is 1
+        # do some smoothing so that minimum count of a word is 1
         if not (word in freqNegative):
             freqNegative[word] = 1
         if not (word in freqPositive):
@@ -128,11 +135,12 @@ def trainBayes(sentencesTrain, pWordPos, pWordNeg, pWord):
         # Calculate p(word)
         pWord[word] = (freqPositive[word] + freqNegative[word]) / float(allWordsTot) 
 
-#---------------------------End Training ----------------------------------
+# ---------------------------End Training ----------------------------------
 
-#implement naive bayes algorithm
-#INPUTS:
-#  sentencesTest is a dictonary with sentences associated with sentiment 
+
+# implement naive bayes algorithm
+# INPUTS:
+#  sentencesTest is a dictionary with sentences associated with sentiment 
 #  dataName is a string (used only for printing output)
 #  pWordPos is dictionary storing p(word|positive) for each word
 #     i.e., pWordPos["apple"] will return a real value for p("apple"|positive)
@@ -140,88 +148,87 @@ def trainBayes(sentencesTrain, pWordPos, pWordNeg, pWord):
 #  pWord is dictionary storing p(word)
 #  pPos is a real number containing the fraction of positive reviews in the dataset
 def testBayes(sentencesTest, dataName, pWordPos, pWordNeg, pWord,pPos):
-    pNeg=1-pPos
+    pNeg = 1-pPos
 
-    #These variables will store results (you do not need them)
-    total=0
-    correct=0
-    totalpos=0
-    totalpospred=0
-    totalneg=0
-    totalnegpred=0
-    correctpos=0
-    correctneg=0
+    # These variables will store results (you do not need them)
+    total = 0
+    correct = 0
+    totalpos = 0
+    totalpospred = 0
+    totalneg = 0
+    totalnegpred = 0
+    correctpos = 0
+    correctneg = 0
 
-    #for each sentence, sentiment pair in the dataset
+    # for each sentence, sentiment pair in the dataset
     for sentence, sentiment in sentencesTest.items():
-        wordList = re.findall(r"[\w']+", sentence)#collect all words
+        wordList = re.findall(r"[\w']+", sentence)  #collect all words
 
-        
-        #TO DO: Exactly what you did in the training function:
-        #Populate bigramList by concatenating adjacent words in wordList.
+        # TODO: Exactly what you did in the training function:
+        # Populate bigramList by concatenating adjacent words in wordList.
 
-        bigramList=wordList.copy() #initialise bigramList
+        bigramList = wordList.copy() #initialise bigramList
         for x in range(len(wordList)-1):
            bigramList.append(wordList[x]+"_" + wordList[x+1])
 
+
 #------------------finished populating bigramList--------------
-        pPosW=pPos
-        pNegW=pNeg
+        pPosW = pPos
+        pNegW = pNeg
 
-        for word in bigramList: #calculate over bigrams
-#        for word in wordList: #calculate over unigrams
+        for word in bigramList:  # calculate over bigrams
+        # for word in wordList:  # calculate over unigrams
             if word in pWord:
-                if pWord[word]>0.00000001:
-                    pPosW *=pWordPos[word]
-                    pNegW *=pWordNeg[word]
+                if pWord[word] > 0.00000001:
+                    pPosW *= pWordPos[word]
+                    pNegW *= pWordNeg[word]
 
-        prob=0;            
-        if pPosW+pNegW >0:
-            prob=pPosW/float(pPosW+pNegW)
+        prob = 0
+        if pPosW+pNegW > 0:
+            prob = pPosW/float(pPosW+pNegW)
 
 
-        total+=1
-        if sentiment=="positive":
-            totalpos+=1
-            if prob>0.5:
-                correct+=1
-                correctpos+=1
-                totalpospred+=1
+        total += 1
+        if sentiment == "positive":
+            totalpos += 1
+            if prob > 0.5:
+                correct += 1
+                correctpos += 1
+                totalpospred += 1
             else:
-                correct+=0
-                totalnegpred+=1
+                correct += 0
+                totalnegpred += 1
                 if PRINT_ERRORS:
-                    print ("ERROR (pos classed as neg %0.2f):" %prob + sentence)
+                    print("ERROR (pos classed as neg %0.2f):" %prob + sentence)
         else:
-            totalneg+=1
-            if prob<=0.5:
-                correct+=1
-                correctneg+=1
-                totalnegpred+=1
+            totalneg += 1
+            if prob <= 0.5:
+                correct += 1
+                correctneg += 1
+                totalnegpred += 1
             else:
-                correct+=0
-                totalpospred+=1
+                correct += 0
+                totalpospred += 1
                 if PRINT_ERRORS:
-                    print ("ERROR (neg classed as pos %0.2f):" %prob + sentence)
+                    print("ERROR (neg classed as pos %0.2f):" %prob + sentence)
  
-    acc=correct/float(total)
-    print (dataName + " Accuracy (All)=%0.2f" % acc + " (%d" % correct + "/%d" % total + ")\n")
+    acc = correct/float(total)
+    print(dataName + " Accuracy (All)=%0.2f" % acc + " (%d" % correct + "/%d" % total + ")\n")
 
     precision_pos=correctpos/float(totalpospred)
     recall_pos=correctpos/float(totalpos)
     precision_neg=correctneg/float(totalnegpred)
     recall_neg=correctneg/float(totalneg)
-    f_pos=2*precision_pos*recall_pos/(precision_pos+recall_pos);
-    f_neg=2*precision_neg*recall_neg/(precision_neg+recall_neg);
+    f_pos = 2*precision_pos*recall_pos/(precision_pos+recall_pos)
+    f_neg = 2*precision_neg*recall_neg/(precision_neg+recall_neg)
 
-    print (dataName + " Precision (Pos)=%0.2f" % precision_pos + " (%d" % correctpos + "/%d" % totalpospred + ")")
-    print (dataName + " Recall (Pos)=%0.2f" % recall_pos + " (%d" % correctpos + "/%d" % totalpos + ")")
-    print (dataName + " F-measure (Pos)=%0.2f" % f_pos)
+    print(dataName + " Precision (Pos)=%0.2f" % precision_pos + " (%d" % correctpos + "/%d" % totalpospred + ")")
+    print(dataName + " Recall (Pos)=%0.2f" % recall_pos + " (%d" % correctpos + "/%d" % totalpos + ")")
+    print(dataName + " F-measure (Pos)=%0.2f" % f_pos)
 
-    print (dataName + " Precision (Neg)=%0.2f" % precision_neg + " (%d" % correctneg + "/%d" % totalnegpred + ")")
-    print (dataName + " Recall (Neg)=%0.2f" % recall_neg + " (%d" % correctneg + "/%d" % totalneg + ")")
-    print (dataName + " F-measure (Neg)=%0.2f" % f_neg + "\n")
-
+    print(dataName + " Precision (Neg)=%0.2f" % precision_neg + " (%d" % correctneg + "/%d" % totalnegpred + ")")
+    print(dataName + " Recall (Neg)=%0.2f" % recall_neg + " (%d" % correctneg + "/%d" % totalneg + ")")
+    print(dataName + " F-measure (Neg)=%0.2f" % f_neg + "\n")
 
 
 # This is a simple classifier that uses a sentiment dictionary to classify 
@@ -230,64 +237,60 @@ def testBayes(sentencesTest, dataName, pWordPos, pWordNeg, pWord,pPos):
 # If the final score is above a threshold, it classifies as "Positive", 
 # otherwise as "Negative"
 def testDictionary(sentencesTest, dataName, sentimentDictionary, threshold):
-    total=0
-    correct=0
-    totalpos=0
-    totalneg=0
-    totalpospred=0
-    totalnegpred=0
-    correctpos=0
-    correctneg=0
+    total = 0
+    correct = 0
+    totalpos = 0
+    totalneg = 0
+    totalpospred = 0
+    totalnegpred = 0
+    correctpos = 0
+    correctneg = 0
     for sentence, sentiment in sentencesTest.items():
         Words = re.findall(r"[\w']+", sentence)
-        score=0
+        score = 0
         for word in Words:
             if word in sentimentDictionary:
-               score+=sentimentDictionary[word]
+               score += sentimentDictionary[word]
  
-        total+=1
-        if sentiment=="positive":
-            totalpos+=1
-            if score>=threshold:
-                correct+=1
-                correctpos+=1
-                totalpospred+=1
+        total += 1
+        if sentiment == "positive":
+            totalpos += 1
+            if score >= threshold:
+                correct += 1
+                correctpos += 1
+                totalpospred += 1
             else:
-                correct+=0
-                totalnegpred+=1
+                correct += 0
+                totalnegpred += 1
         else:
-            totalneg+=1
-            if score<threshold:
-                correct+=1
-                correctneg+=1
-                totalnegpred+=1
+            totalneg += 1
+            if score < threshold:
+                correct += 1
+                correctneg += 1
+                totalnegpred += 1
             else:
-                correct+=0
-                totalpospred+=1
+                correct += 0
+                totalpospred += 1
  
-    acc=correct/float(total)
-    print (dataName + " Accuracy (All)=%0.2f" % acc + " (%d" % correct + "/%d" % total + ")\n")
-    precision_pos=correctpos/float(totalpospred)
-    recall_pos=correctpos/float(totalpos)
-    precision_neg=correctneg/float(totalnegpred)
-    recall_neg=correctneg/float(totalneg)
-    f_pos=2*precision_pos*recall_pos/(precision_pos+recall_pos);
-    f_neg=2*precision_neg*recall_neg/(precision_neg+recall_neg);
+    acc = correct/float(total)
+    print(dataName + " Accuracy (All)=%0.2f" % acc + " (%d" % correct + "/%d" % total + ")\n")
+    precision_pos = correctpos/float(totalpospred)
+    recall_pos = correctpos/float(totalpos)
+    precision_neg = correctneg/float(totalnegpred)
+    recall_neg = correctneg/float(totalneg)
+    f_pos = 2*precision_pos*recall_pos/(precision_pos+recall_pos)
+    f_neg = 2*precision_neg*recall_neg/(precision_neg+recall_neg)
+
+    print(dataName + " Precision (Pos)=%0.2f" % precision_pos + " (%d" % correctpos + "/%d" % totalpospred + ")")
+    print(dataName + " Recall (Pos)=%0.2f" % recall_pos + " (%d" % correctpos + "/%d" % totalpos + ")")
+    print(dataName + " F-measure (Pos)=%0.2f" % f_pos)
+
+    print(dataName + " Precision (Neg)=%0.2f" % precision_neg + " (%d" % correctneg + "/%d" % totalnegpred + ")")
+    print(dataName + " Recall (Neg)=%0.2f" % recall_neg + " (%d" % correctneg + "/%d" % totalneg + ")")
+    print(dataName + " F-measure (Neg)=%0.2f" % f_neg + "\n")
 
 
-    print (dataName + " Precision (Pos)=%0.2f" % precision_pos + " (%d" % correctpos + "/%d" % totalpospred + ")")
-    print (dataName + " Recall (Pos)=%0.2f" % recall_pos + " (%d" % correctpos + "/%d" % totalpos + ")")
-    print (dataName + " F-measure (Pos)=%0.2f" % f_pos)
-
-    print (dataName + " Precision (Neg)=%0.2f" % precision_neg + " (%d" % correctneg + "/%d" % totalnegpred + ")")
-    print (dataName + " Recall (Neg)=%0.2f" % recall_neg + " (%d" % correctneg + "/%d" % totalneg + ")")
-    print (dataName + " F-measure (Neg)=%0.2f" % f_neg + "\n")
-
-
-
-
-
-#Print out n most useful predictors
+# Print out n most useful predictors
 def mostUseful(pWordPos, pWordNeg, pWord, n):
     predictPower={}
     for word in pWord:
@@ -299,48 +302,45 @@ def mostUseful(pWordPos, pWordNeg, pWord, n):
 
     sortedPower = sorted(predictPower, key=predictPower.get)
     head, tail = sortedPower[:n], sortedPower[len(predictPower)-n:]
-    print ("NEGATIVE:")
-    print (head)
-    print ("\nPOSITIVE:")
-    print (tail)
+    print("NEGATIVE:")
+    print(head)
+    print("\nPOSITIVE:")
+    print(tail)
 
 
+# ---------- Main Script --------------------------
 
 
-#---------- Main Script --------------------------
+sentimentDictionary = {}  # {} initialises a dictionary [hash function]
+sentencesTrain = {}
+sentencesTest = {}
+sentencesNokia = {}
 
-
-sentimentDictionary={} # {} initialises a dictionary [hash function]
-sentencesTrain={}
-sentencesTest={}
-sentencesNokia={}
-
-#initialise datasets and dictionaries
+# initialise datasets and dictionaries
 readFiles(sentimentDictionary,sentencesTrain,sentencesTest,sentencesNokia)
 
-pWordPos={} # p(W|Positive)
-pWordNeg={} # p(W|Negative)
-pWord={}    # p(W) 
+pWordPos = {}  # p(W|Positive)
+pWordNeg = {}  # p(W|Negative)
+pWord = {}     # p(W) 
 
-#build conditional probabilities using training data
+# build conditional probabilities using training data
 trainBayes(sentencesTrain, pWordPos, pWordNeg, pWord)
 
-#run naive bayes classifier on datasets
-#print ("Naive Bayes")
-#testBayes(sentencesTrain,  "Films (Train Data, Naive Bayes)\t", pWordPos, pWordNeg, pWord,0.5)
+# run naive bayes classifier on datasets
+# print("Naive Bayes")
+# testBayes(sentencesTrain,  "Films (Train Data, Naive Bayes)\t", pWordPos, pWordNeg, pWord,0.5)
 testBayes(sentencesTest,  "Films  (Test Data, Naive Bayes)\t", pWordPos, pWordNeg, pWord,0.5)
-#testBayes(sentencesNokia, "Nokia   (All Data,  Naive Bayes)\t", pWordPos, pWordNeg, pWord,0.7)
+# testBayes(sentencesNokia, "Nokia   (All Data,  Naive Bayes)\t", pWordPos, pWordNeg, pWord,0.7)
 
 
-
-#run sentiment dictionary based classifier on datasets
+# run sentiment dictionary based classifier on datasets
 # testDictionary(sentencesTrain,  "Films (Train Data, Rule-Based)\t", sentimentDictionary, -4)
 # testDictionary(sentencesTest,  "Films  (Test Data, Rule-Based)\t",  sentimentDictionary, -4)
 # testDictionary(sentencesNokia, "Nokia   (All Data, Rule-Based)\t",  sentimentDictionary, -3)
 
 
 # print most useful words
-#mostUseful(pWordPos, pWordNeg, pWord, 50)
+# mostUseful(pWordPos, pWordNeg, pWord, 50)
 
 
 
